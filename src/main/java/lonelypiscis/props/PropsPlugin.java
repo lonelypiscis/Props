85a53c0bb5e73d17b56624c94e71b2e3aadfeb51
lonelypiscis <gurokrei@gmail.com>
2017-11-29 16:21:00 +0100

    Initial commit

--- src/main/java/lonelypiscis/props/PropsPlugin.java
+++ src/main/java/lonelypiscis/props/PropsPlugin.java
@@ -1,8 +1,13 @@
 package lonelypiscis.props;
 
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 @Plugin(id = "props", name = "Props Plugin", version = "0.1")
 public class PropsPlugin {
-
+	@Listener
+	public void onServerStart(GameStartedServerEvent evt) {
+		System.out.println("Starting!");
+	}
 }
